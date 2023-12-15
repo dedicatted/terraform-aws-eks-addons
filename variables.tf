4b683ae905ed0b45e61b11b1f0330540c77165ef
Bohdan Mukovozov <bmukovozovb@dedicatted.com>
2023-12-15 12:17:24 +0200

    modify variables name in cert manager addons

--- variables.tf
+++ variables.tf
@@ -176,43 +176,43 @@ variable "cert_manager_enabled" {
   description = "Variable indicating whether deployment is enabled."
 }
 
-variable "helm_chart_name" {
+variable "cert_manager_helm_chart_name" {
   type        = string
   default     = "cert-manager"
   description = "Cert Manager Helm chart name to be installed"
 }
 
-variable "helm_chart_release_name" {
+variable "cert_manager_helm_chart_release_name" {
   type        = string
   default     = "cert-manager"
   description = "Helm release name"
 }
 
-variable "helm_chart_version" {
+variable "cert_manager_helm_chart_version" {
   type        = string
   default     = "1.1.0"
   description = "Cert Manager Helm chart version."
 }
 
-variable "helm_chart_repo" {
+variable "cert_manager_helm_chart_repo" {
   type        = string
   default     = "https://charts.jetstack.io"
   description = "Cert Manager repository name."
 }
 
-variable "install_CRDs" {
+variable "cert_manager_install_CRDs" {
   type        = bool
   default     = true
   description = "To automatically install and manage the CRDs as part of your Helm release."
 }
 
-variable "create_namespace" {
+variable "cert_manager_create_namespace" {
   type        = bool
   default     = true
   description = "Whether to create Kubernetes namespace with name defined by `namespace`."
 }
 
-variable "settings" {
+variable "cert_manager_settings" {
   default     = {}
   description = "Additional settings which will be passed to the Helm chart values."
 }
